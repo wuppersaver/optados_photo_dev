5d10e9aa7b0e7adbe3fa9e61492f11e022668d37
Felix Mildner <felix@wuppersaver.de>
2025-02-07 22:32:18 +0000

    Added conversions in od2od for transmission probabilities

--- optados/src/od2od.f90
+++ optados/src/od2od.f90
@@ -5,7 +5,8 @@ module od_conv
   use od_constants, only: dp
   use od_electronic, only: elec_read_optical_mat, elec_read_band_gradient, elec_read_elnes_mat,&
        & elec_pdos_read, elec_read_band_energy, omefile_header, domefile_header, pdosfile_header,&
-       & elnesfile_header, elec_read_foptical_mat, femfile_header, fem_energy_info, tmcoeff_file_header
+       & elnesfile_header, elec_read_foptical_mat, femfile_header, fem_energy_info, tmprob_file_header, &
+       & elec_read_transmit_prob
   use od_parameters, only: iprint
   use od_io, only: stdout, io_error, seedname
   implicit none
@@ -36,8 +37,8 @@ contains
     ! Added by F. Mildner (04/2023+02/2025) for photoemission
     write (stdout, '(A)') "       fem_fmt : a formatted free electron optical matrix element file"
     write (stdout, '(A)') "       fem_bin : an unformatted free electron optical matrix element file"
-    write (stdout, '(A)') "       tmcoeff_fmt : a formatted bandwise electron transmission coefficient file"
-    write (stdout, '(A)') "       tmcoeff_bin : an unformatted bandwise electron transmission coefficient file"
+    write (stdout, '(A)') "   tmprob_fmt : a formatted bandwise electron transmission coefficient file"
+    write (stdout, '(A)') "   tmprob_bin : an unformatted bandwise electron transmission coefficient file"
 
     write (stdout, '(A)') "      dome_fmt : a formatted diagonal optical matrix element file"
     write (stdout, '(A)') "      dome_bin : an unformatted diagonal optical matrix element file"
@@ -412,13 +413,13 @@ contains
 
   !=========================================================================
   !=========================================================================
-  subroutine read_tmcoeff_fmt()
+  subroutine read_tmprob_fmt()
     !! Read a formatted Optical Matrix Elements file.
     use od_constants, only: dp, bohr2ang, H2eV
     use od_io, only: io_time, filename_len, seedname, stdout, io_file_unit,&
          & io_error
     use od_cell, only: num_kpoints_on_node, nkpoints
-    use od_electronic, only: nspins, nbands, transmit_coeff
+    use od_electronic, only: nspins, nbands, transmit_prob
     use od_constants, only: bohr2ang, H2eV
     implicit none
 
@@ -426,18 +427,18 @@ contains
     character(len=100):: string, string2
     integer :: ik, is, ib, i, jb, energy_count, ierr, tmcoeff_unit = 6
 
-    write (stdout, *) " Read a formatted .tmcoeff file. "
+    write (stdout, *) " Read a formatted .tmprob file. "
 
-    open (unit=tmcoeff_unit, form='formatted', recl=1073741824, file=trim(seedname)//".tmcoeff_fmt")
+    open (unit=tmcoeff_unit, form='formatted', recl=1073741824, file=trim(seedname)//".tmprob_fmt")
     read (tmcoeff_unit, '('//trim(format_precision)//')') file_version
 
-    read (tmcoeff_unit, '(a80)') tmcoeff_file_header
+    read (tmcoeff_unit, '(a80)') tmprob_file_header
 
-    if (.not. allocated(transmit_coeff)) then
+    if (.not. allocated(transmit_prob)) then
       write (stdout, *) " Allocating transmit_coeffs."
-      allocate (transmit_coeff(nbands, nspins, nkpoints), stat=ierr)
+      allocate (transmit_prob(nbands, nspins, nkpoints), stat=ierr)
     end if
-    ! ! Total number of elements of tmcoeff
+    ! ! Total number of elements of tmprob
     ! write(stdout,*) 'nbands', nbands
     write (string, '(I0,"(1x,",a,")")') nbands, trim(format_precision)
     ! write(stdout,*) string
@@ -447,24 +448,24 @@ contains
 
     do ik = 1, nkpoints
       do is = 1, nspins
-        read (tmcoeff_unit, '('//trim(string)//')') (transmit_coeff(ib, ik, is), ib=1, nbands)
+        read (tmcoeff_unit, '('//trim(string)//')') (transmit_prob(ib, ik, is), ib=1, nbands)
       end do
     end do
 
     close (unit=tmcoeff_unit)
 
-    write (stdout, *) trim(seedname)//".tmcoeff_fmt"//"--> Formatted tmcoeff sucessfully read. "
+    write (stdout, *) trim(seedname)//".tmprob_fmt"//"--> Formatted tmprob sucessfully read. "
 
-  end subroutine read_tmcoeff_fmt
+  end subroutine read_tmprob_fmt
 
   !=========================================================================
-  subroutine write_tmcoeff_fmt()
+  subroutine write_tmprob_fmt()
     !! Write a formatted ome file.
     use od_constants, only: dp, bohr2ang, H2eV
     use od_io, only: io_time, filename_len, seedname, stdout, io_file_unit,&
          & io_error
     use od_cell, only: num_kpoints_on_node, nkpoints
-    use od_electronic, only: nspins, nbands, transmit_coeff
+    use od_electronic, only: nspins, nbands, transmit_prob
     use od_constants, only: bohr2ang, H2eV
     implicit none
 
@@ -472,48 +473,48 @@ contains
     character(len=100):: string
     integer :: ik, is, ib, i, jb, tmcoeff_unit = 6
     
-    write (stdout, *) " Write a formatted .tmcoeff file. "
+    write (stdout, *) " Write a formatted .tmprob file. "
 
-    open (unit=tmcoeff_unit, form='formatted', file=trim(outseedname)//".tmcoeff_fmt")
+    open (unit=tmcoeff_unit, form='formatted', file=trim(outseedname)//".tmprob_fmt")
 
     write (string, '(I0,"(1x,",a,")")') nbands, trim(format_precision)
     ! write(stdout, *) string
 
-    write (stdout, '(a80)') tmcoeff_file_header
-    write (stdout, '(a80)') adjustl(tmcoeff_file_header)
+    write (stdout, '(a80)') tmprob_file_header
+    write (stdout, '(a80)') adjustl(tmprob_file_header)
 
     write (tmcoeff_unit, '('//trim(format_precision)//')') file_version
-    write (tmcoeff_unit, '(a80)') adjustl(tmcoeff_file_header)
+    write (tmcoeff_unit, '(a80)') adjustl(tmprob_file_header)
 
     do ik = 1, nkpoints
       do is = 1, nspins
-        write (tmcoeff_unit, '('//trim(string)//')') (transmit_coeff(ib, ik, is), ib=1, nbands)
+        write (tmcoeff_unit, '('//trim(string)//')') (transmit_prob(ib, ik, is), ib=1, nbands)
       end do
     end do
 
     close (unit=tmcoeff_unit)
 
-    write (stdout, *) " Sucesfully written a formatted tmcoeff file --> "//trim(outseedname)//".tmcoeff_fmt"
-  end subroutine write_tmcoeff_fmt
+    write (stdout, *) " Sucesfully written a formatted tmprob file --> "//trim(outseedname)//".tmprob_fmt"
+  end subroutine write_tmprob_fmt
 
   !=========================================================================
-  subroutine read_tmcoeff_bin()
+  subroutine read_tmprob_bin()
     !! Read a binary ome file. Wrapper to keep the naming tidy.
     implicit none
-    write (stdout, *) " Read a formatted tmcoeff file. "
+    write (stdout, *) " Read an unformatted tmprob file. "
 
-    call elec_read_transmit_coeff()
-    write (stdout, *) " "//trim(seedname)//".tmcoeff_bin"//"--> Unformatted tmcoeff sucessfully read. "
-  end subroutine read_tmcoeff_bin
+    call elec_read_transmit_prob()
+    write (stdout, *) " "//trim(seedname)//".tmprob_bin"//"--> Unformatted tmprob sucessfully read. "
+  end subroutine read_tmprob_bin
 
   !=========================================================================
-  subroutine write_tmcoeff_bin()
+  subroutine write_tmprob_bin()
     !! Write a binary ome file.
     use od_constants, only: dp, bohr2ang, H2eV
     use od_io, only: io_time, filename_len, seedname, stdout, io_file_unit,&
          & io_error
     use od_cell, only: num_kpoints_on_node, nkpoints
-    use od_electronic, only: nspins, nbands, transmit_coeff
+    use od_electronic, only: nspins, nbands, transmit_prob
     use od_constants, only: bohr2ang, H2eV
     implicit none
 
@@ -521,24 +522,24 @@ contains
     character(len=100):: string
     integer :: ik, is, ib, i, jb,energy_count, tmcoeff_unit = 6
 
-    write (stdout, *) " Write a binary tmcoeff file."
+    write (stdout, *) " Write a binary tmprob file."
 
-    open (unit=tmcoeff_unit, form='unformatted', file=trim(outseedname)//".tmcoeff_bin")
+    open (unit=tmcoeff_unit, form='unformatted', file=trim(outseedname)//".tmprob_bin")
 
-    write (stdout, *) "-> TmCoeffFile_version ", file_version
+    write (stdout, *) "-> tmprob file_version ", file_version
     write (tmcoeff_unit) file_version
-    write (stdout, *) "-> TmCoeffFile_header ", trim(tmcoeff_file_header)
-    write (tmcoeff_unit) adjustl(tmcoeff_file_header)
+    write (stdout, *) "-> tmprob file_header ", trim(tmprob_file_header)
+    write (tmcoeff_unit) adjustl(tmprob_file_header)
 
     ! write(0,*) nkpoints, nspins, nbands
     do ik = 1, nkpoints
       do is = 1, nspins
-        write (tmcoeff_unit) (transmit_coeff(ib, ik, is), ib=1, nbands)
+        write (tmcoeff_unit) (transmit_prob(ib, ik, is), ib=1, nbands)
       end do
     end do
 
-    write (stdout, *) " Sucesfully written an unformatted tmcoeff file --> "//trim(outseedname)//".tmcoeff_bin"
-  end subroutine write_tmcoeff_bin
+    write (stdout, *) " Sucesfully written an unformatted tmprob file --> "//trim(outseedname)//".tmprob_bin"
+  end subroutine write_tmprob_bin
 
   !=========================================================================
   ! D I A G O N A L  O P T I C A L   M A T R I X   E L E M E N T S
@@ -1245,7 +1246,7 @@ program od2od
   implicit none
 
   logical :: file_found
-  logical :: ome_conv, fem_conv, dome_conv, pdos_conv, elnes_conv, dummy_conv
+  logical :: ome_conv, fem_conv, tmcoeff_conv, dome_conv, pdos_conv, elnes_conv, dummy_conv
   !! Flags to stop people trying to, say, read in a pdos and write out an
   !! elnes. That's not going to end well.
   real(kind=dp) :: time0, time1
@@ -1308,6 +1309,7 @@ program od2od
 
   ome_conv = .false.
   fem_conv = .false.
+  tmcoeff_conv = .false.
   dome_conv = .false.
   pdos_conv = .false.
   elnes_conv = .false.
@@ -1335,6 +1337,16 @@ case ("fem_bin")
   call get_band_energy()
   call write_read_file()
   call read_fem_bin()
+case ("tmprob_fmt")
+  tmcoeff_conv = .true.
+  call get_band_energy()
+  call write_read_file()
+  call read_tmprob_fmt()
+case ("tmprob_bin")
+  tmcoeff_conv = .true.
+  call get_band_energy()
+  call write_read_file()
+  call read_tmprob_bin()
 case ("dome_fmt")
   dome_conv = .true.
   call get_band_energy()
@@ -1396,6 +1408,14 @@ case ("fem_bin")
   if (.not. (fem_conv)) call io_error(' Input format '//trim(infile)//' not compatible with output format '&
        &//trim(outfile))
   call write_fem_bin()
+case ("tmprob_fmt")
+  if (.not. (tmcoeff_conv)) call io_error(' Input format '//trim(infile)//' not compatible with output format'&
+       &//trim(outfile))
+  call write_tmprob_fmt
+case ("tmprob_bin")
+  if (.not. (tmcoeff_conv)) call io_error(' Input format '//trim(infile)//' not compatible with output format'&
+       &//trim(outfile))
+  call write_tmprob_bin
 case ("dome_fmt")
   if (.not. (dome_conv .or. ome_conv)) call io_error(' Input format '//trim(infile)//&
        &' not compatible with output format '//trim(outfile))
