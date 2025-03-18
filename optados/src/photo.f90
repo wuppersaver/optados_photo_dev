5be9f523b11194acf399459b835a30144d459e11
Felix Mildner <felix@wuppersaver.de>
2025-03-18 10:20:45 +0000

    Earlier dellocation of Etrans array should reduce memory requirement

--- optados/src/photo.f90
+++ optados/src/photo.f90
@@ -3591,8 +3591,8 @@ contains
         mean_te = 0.0_dp
       end if
 
-      ! deallocate (te_tsm_temp, stat=ierr)
-      ! if (ierr /= 0) call io_error('Error: weighted_mean_te - failed to deallocate te_tsm_temp')
+      deallocate (te_tsm, stat=ierr)
+      if (ierr /= 0) call io_error('Error: weighted_mean_te - failed to deallocate te_tsm')
 
       deallocate (layer_e_transverse, stat=ierr)
       if (ierr /= 0) call io_error('Error: weighted_mean_te - failed to deallocate layer_e_transverse')
@@ -3653,8 +3653,8 @@ contains
         mean_te = 0.0_dp
       end if
 
-      ! deallocate (te_osm_temp, stat=ierr)
-      ! if (ierr /= 0) call io_error('Error: weighted_mean_te - failed to deallocate te_osm_temp')
+      deallocate (te_osm, stat=ierr)
+      if (ierr /= 0) call io_error('Error: weighted_mean_te - failed to deallocate te_osm')
 
     else if (index(photo_model, 'ds_like_pe') > 0) then
 
