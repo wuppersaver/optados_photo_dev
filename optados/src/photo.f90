d250cbe9e96db23a1da533d482129a6e349a1def
wuppersaver <felix@wuppersaver.de>
2025-03-28 00:53:24 +0000

    Fixed issues with summation into optimised array A simple order of operations issue - solved with a few brackets around the addition

--- optados/src/photo.f90
+++ optados/src/photo.f90
@@ -65,7 +65,7 @@ module od_photo
   real(kind=dp), allocatable, dimension(:, :, :, :) :: E_kinetic
   real(kind=dp), allocatable, dimension(:, :, :, :) :: E_transverse
   real(kind=dp), allocatable, dimension(:) :: bind_energy
-  real(kind=dp), allocatable, dimension(:, :, :) :: weighted_temp_atom
+  real(kind=dp), allocatable, dimension(:, :) :: weighted_temp_atom
   ! real(kind=dp), allocatable, dimension(:, :, :, :, :) :: weighted_temp
   integer :: max_energy = -1
   real(kind=dp), allocatable, dimension(:, :, :, :)    :: qe_osm
@@ -3236,7 +3236,7 @@ contains
     if (ierr /= 0) call io_error('Error: binding_energy_broadening - allocation of bind_energy failed')
     bind_energy = 0.0_dp
 
-    allocate (weighted_temp_atom(max_energy, num_kpoints_on_node(my_node_id), max_atoms + 1), stat=ierr)
+    allocate (weighted_temp_atom(max_energy, max_atoms + 1), stat=ierr)
     if (ierr /= 0) call io_error('Error: binding_energy_broadening - allocation of weighted_temp_atom failed')
     weighted_temp_atom = 0.0_dp
 
@@ -3345,9 +3345,10 @@ contains
                                     *transverse_gauss(gdx, n_eigen_init, N_spin, N_k)                   
                   do e_scale = max(middle_idx - width_idx, 1), min(middle_idx + width_idx, max_energy)
                   ! do e_scale = 1, max_energy
-                    weighted_temp_atom(e_scale, N_k, atom) =  weighted_temp_atom(e_scale, N_k, atom) &
-                                                        +binding_temp(e_scale, n_eigen_init, N_spin, N_k) &
-                                                        *temp_contribution*spectral_factor
+                    weighted_temp_atom(e_scale, atom) =  &
+                                                        weighted_temp_atom(e_scale, atom) &
+                                                        +(binding_temp(e_scale, n_eigen_init, N_spin, N_k) &
+                                                        *temp_contribution*spectral_factor)
                   end do
                 end do
               end do
@@ -3386,9 +3387,10 @@ contains
                                   *transverse_gauss(gdx, n_eigen_init, N_spin, N_k)
                 do e_scale = max(middle_idx - width_idx, 1), min(middle_idx + width_idx, max_energy)
                 ! do e_scale = 1, max_energy
-                  weighted_temp_atom(e_scale, N_k, max_atoms + 1) = weighted_temp_atom(e_scale, N_k, max_atoms + 1) &
-                                                               +binding_temp(e_scale, n_eigen_init, N_spin, N_k) &
-                                                               *temp_contribution*spectral_factor
+                  weighted_temp_atom(e_scale, max_atoms + 1) =  &
+                                                            weighted_temp_atom(e_scale, max_atoms + 1) &
+                                                            +(binding_temp(e_scale, n_eigen_init, N_spin, N_k) &
+                                                            *temp_contribution*spectral_factor)
                 end do
               end do
             end do
@@ -3474,9 +3476,10 @@ contains
                                   *transverse_gauss(gdx, n_eigen, N_spin, N_k)
                 do e_scale = max(middle_idx - width_idx, 1), min(middle_idx + width_idx, max_energy)
                 ! do e_scale = 1, max_energy
-                  weighted_temp_atom(e_scale, N_k, atom) = weighted_temp_atom(e_scale, N_k, atom)&
-                                                +binding_temp(e_scale, n_eigen, N_spin, N_k) &
-                                                *temp_contribution*spectral_factor
+                  weighted_temp_atom(e_scale, atom) = & 
+                                                weighted_temp_atom(e_scale, atom) &
+                                                +(binding_temp(e_scale, n_eigen, N_spin, N_k) &
+                                                *temp_contribution*spectral_factor)
                 end do
               end do
             end do
@@ -3620,7 +3623,7 @@ contains
       qe_atom = 0.0_dp
       do e_scale = 1, max_energy !loop over binding energy
         do atom = 1, max_atoms + 1
-          qe_atom(atom, e_scale) = sum(weighted_temp_atom(e_scale, 1:num_kpoints_on_node(my_node_id), atom))
+          qe_atom(atom, e_scale) = weighted_temp_atom(e_scale, atom)
         end do
       end do
 
