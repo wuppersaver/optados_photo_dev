0fa33d326c7d0f219a3775241091ce1b74e21705
Felix Mildner <felix@wuppersaver.de>
2026-09-02 20:49:25 +0100

    Read the Fermi energy from a .bands header list-directed
    The value was read with an f12.4 edit descriptor, which silently truncates
    or misreads when the number does not fit that width.  Read it
    list-directed, as the surrounding fields already are.

--- optados/src/electronic.f90
+++ optados/src/electronic.f90
@@ -995,7 +995,7 @@ contains
       read (dummy(str_pos + 11:), *) nbands
       read (band_unit, '(a)') dummy
       str_pos = index(dummy, 'units)')
-      read (dummy(str_pos + 6:), '(f12.4)') efermi_castep
+      read (dummy(str_pos + 6:), *) efermi_castep
       read (band_unit, '(a)') dummy
       read (band_unit, *) real_lattice(:, 1)
       read (band_unit, *) real_lattice(:, 2)
@@ -1174,7 +1174,7 @@ contains
       read (dummy(str_pos + 11:), *) nbands
       read (band_unit, '(a)') dummy
       str_pos = index(dummy, 'units)')
-      read (dummy(str_pos + 6:), '(f12.4)') efermi_castep
+      read (dummy(str_pos + 6:), *) efermi_castep
       read (band_unit, '(a)') dummy
       read (band_unit, *) real_lattice(:, 1)
       read (band_unit, *) real_lattice(:, 2)
